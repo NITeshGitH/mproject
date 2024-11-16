effd5a0a97d0a42c6514561f19f2c59b275b380a
Nitesh Verma <117501549+NITeshGitH@users.noreply.github.com>
2024-11-16 18:45:05 +0530

    Update Footer.jsx

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,4 +1,6 @@
 import { Fot1, Fot2 } from ".";
+import Image from "next/image";
+import images from "../Images/index";
 
 export default () => {
   const footerNavs = [
@@ -24,10 +26,16 @@ export default () => {
       <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
         <div className="justify-between sm:flex">
           <div className="space-y-6">
-            <img src="https://www.floatui.com/logo.svg" className="w-32" alt="Logo"/>
+          <Image
+                className="w-28"
+                src={images.logo}
+                alt="Logo"
+              />
             <p className="max-w-md">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus quibusdam dolor exercitationem et, nihil soluta suscipit consectetur veritatis iste, ab, sequi doloribus. Veritatis amet facere debitis officia praesentium minima culpa!
-            </p>
+            Streamline your supply chain operations with our blockchain-based platform. 
+              Create, track, and complete shipments with unparalleled transparency, security, 
+              and efficiency. Revolutionize how you manage logistics with end-to-end visibility 
+              and smart contract technology.</p>
             <ul className="flex flex-wrap items-center gap-4 text-sm sm:text-base">
               {footerNavs.map((item, idx) => (
                 <li className="text-gray-800 hover:text-gray-500 duration-150">
